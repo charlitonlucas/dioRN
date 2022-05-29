8464a39bff4eb6bbd69a164b1bb9146c44a0567c
CHARLITONLUCAS <charlitonlessa@gmail.com>
2022-05-29 12:21:22 -0300

    PRIMEIRO COMMIT- DIO-REACT NATIVE

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View , Text} from "react-native";
+
+const app = () => {
+  return (
+    <View>
+      <Text>Esse é o meu primeiro App</Text>
+    </View>
+  );
+};
+export default app;
